4513535bafa042cc6f339a0c993a894e2700a964
laurette001 <167814443+laurette001@users.noreply.github.com>
2024-04-23 16:36:41 -0300

    Criada a Classe Contato cs
    Ciada a classe contatocs e suas propriedades como nome, sobrenome e telefone

---
+++ WindowsFormsApp1/Contato.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Contato
+    {
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+        public string Sobrenome
+        {
+            get
+            {
+                return sobrenome;
+            }
+            set
+            {
+                sobrenome = value;
+            }
+        }
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                if (value.Length ==13)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "(00) 00000-0000";
+                }
+            } 
+        }
+
+        public contato()
+        {
+            Nome= "Cláudia"
+        }
+    }
+}
+         
